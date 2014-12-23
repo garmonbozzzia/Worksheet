2d598d86c4ad11e64133cad9efc3401e4dfb6508
Yury Ignatev <yury.ignatev@gmail.com>
2014-12-24 03:34:08 +0400

    TypeSwitch

---
+++ Utils/TypeSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public class TypeSwitch
+    {
+        Dictionary<Type, Action<object>> matches = new Dictionary<Type, Action<object>>(); 
+
+        public TypeSwitch Case<T>(Action<T> action)
+        {
+            matches[typeof (T)] = (x) => action((T)x);
+            return this;
+        }
+
+        public void Switch<T>(T arg)
+        {
+            matches[typeof (T)](arg);
+        }
+    }
+}
