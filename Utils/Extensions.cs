6ef11e2b11584b47c938cd915fc1a67246b728e3
Yury Ignatev <yury.ignatev@gmail.com>
2014-12-24 02:39:37 +0400

    Extensions
    Cons
    Concat
    ListWrap
    UnwrapList
    Repeat
    ForEach
    Next
    TakeWhileIncluding
    To

---
+++ Utils/Extensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> Cons<T>(this T x, IEnumerable<T> xs)
+        {
+            return x.ListWrap().Concat(xs);
+        }
+
+        public static IEnumerable<T> Concat<T>(this IEnumerable<IEnumerable<T>> xs)
+        {
+            return xs.SelectMany(x => x);
+        }
+
+        public static IEnumerable<T> ListWrap<T>(this T x)
+        {
+            yield return x;
+        }
+
+        public static IEnumerable<T> UnwrapList<T>(this IEnumerable<IEnumerable<T>> xss)
+        {
+            return xss.SelectMany(xs => xs);
+        }
+
+        public static IEnumerable<T> Repeat<T>(this IEnumerable<T> xs, int repeat = int.MaxValue)
+        {
+            return Enumerable.Range(1, repeat).SelectMany(_ => xs);
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> xs, Action<T> action)
+        {
+            foreach (var x in xs)
+                action(x);
+        }
+
+        public static IEnumerable<T> Next<T>(this IEnumerable<T> xs, T x)
+        {
+            return xs.Concat(x.ListWrap());
+        }
+
+        public static IEnumerable<T> Next<T>(this T x, T y)
+        {
+            return x.ListWrap().Next(y);
+        }
+
+        public static IEnumerable<T> TakeWhileIncluding<T>(this IEnumerable<T> xs, Func<T, bool> p)
+        {
+            foreach (var x in xs)
+            {
+                yield return x;
+                if (!p(x))
+                    yield break;
+            }
+        }
+
+        public static IEnumerable<int> To(this int number, int to)
+        {
+            return Enumerable.Range(number, to - number + 1);
+        }
+
+        public static IEnumerable<int> To(this int number)
+        {
+            return Enumerable.Range(number, int.MaxValue);
+        }
+    }
+}
